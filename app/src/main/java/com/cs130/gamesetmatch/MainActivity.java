4ebc9f6d50a0903e51e4c190936e363001a23a86
graciefulgrace <graciefulgrace@ucla.edu>
2015-05-07 21:45:05 -0700

    #3 Game: Drag-n-drop functionality in DragActivity

--- app/src/main/java/com/cs130/gamesetmatch/MainActivity.java
+++ app/src/main/java/com/cs130/gamesetmatch/MainActivity.java
@@ -1,9 +1,17 @@
 package com.cs130.gamesetmatch;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Base64;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 
 public class MainActivity extends ActionBarActivity {
