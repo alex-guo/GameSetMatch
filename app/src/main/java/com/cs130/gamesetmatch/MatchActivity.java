d91bf24b7fbad1372ea7f4fc43cf9572d9bf0202
jeannieanguyen <jeannie.jujubee@gmail.com>
2015-05-28 16:46:36 -0700

    Fix button on Match Activity so it redirects to dashboard.

--- app/src/main/java/com/cs130/gamesetmatch/MatchActivity.java
+++ app/src/main/java/com/cs130/gamesetmatch/MatchActivity.java
@@ -1,5 +1,6 @@
 package com.cs130.gamesetmatch;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -11,46 +12,30 @@ import android.widget.Button;
 import com.facebook.Profile;
 
 
-public class MatchActivity extends ActionBarActivity {
-
+public class MatchActivity extends Activity {
+    private Profile currentProfile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_match);
 
 
-        Button dashButton = (Button) findViewById(R.id.backToDash_button);
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            currentProfile = (Profile) extras.get("currentProfile");
+            Button dashButton = (Button) findViewById(R.id.backToDash_button);
 
-        dashButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MatchActivity.this, DashboardActivity.class);
-                startActivity(intent);
-            }
-        });
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_match, menu);
-
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            dashButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MatchActivity.this, DashboardActivity.class);
+                    intent.putExtra("currentProfile", currentProfile);
+                    intent.putExtra("haveID", "true");
+                    startActivity(intent);
+                }
+            });
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
+
 }
